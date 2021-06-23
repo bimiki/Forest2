7adedef5703484d7c04387eed19b82ba150b6e09
bimiki <noahtamulonis@gmail.com>
2021-06-23 16:23:04 -0400

    stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
 
   def show
-
     @user = User.find(params[:id])
     @user_notes = @user.notes
     @user_profile = @user.profile
     @user_profile_picture = @user.picture
-
   end
+
+  def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+   end
+
 
 end
